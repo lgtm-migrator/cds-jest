941acdd92966d08e69a89785817eb0205685cd73
Theo Sun <theo.sun@outlook.com>
2022-03-07 14:22:32 +0800

    fix: ut for 12

--- test/sample-app/srv/InformationService.js
+++ test/sample-app/srv/InformationService.js
@@ -3,7 +3,7 @@ module.exports = class InformationService extends cds.ApplicationService {
 
   async init() {
     this.before("CREATE", "Information", (req) => {
-      if (req.data?.Value?.length < 10) {
+      if (req.data.Value.length < 10) {
         return req.reject(400, "VALUE_LEN_NOT_ENOUGH")
       }
       return
